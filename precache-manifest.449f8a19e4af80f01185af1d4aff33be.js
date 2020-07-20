c0a2a73ca28a67d55daf7c0baf97dd6a3edbe899
Hanuvendra Pandey <hanuvendrap2014@gmail.com>
2020-07-21 01:07:06 +0530

    Updates

---
+++ precache-manifest.449f8a19e4af80f01185af1d4aff33be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5349ce3e7a542b4ca6132a0569c091b1",
+    "url": "/React-To-do-App/index.html"
+  },
+  {
+    "revision": "d55fbc20bbffc2e55953",
+    "url": "/React-To-do-App/static/css/main.c16efac3.chunk.css"
+  },
+  {
+    "revision": "959923497815b852b391",
+    "url": "/React-To-do-App/static/js/2.a8532e8d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-To-do-App/static/js/2.a8532e8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d55fbc20bbffc2e55953",
+    "url": "/React-To-do-App/static/js/main.30e11930.chunk.js"
+  },
+  {
+    "revision": "972fa576af20b702cea1",
+    "url": "/React-To-do-App/static/js/runtime-main.140a618f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-To-do-App/static/media/logo.5d5d9eef.svg"
+  }
+]);
